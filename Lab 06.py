8c2bf4c5c412f58d3841f49596d2fdaad71f6b5c
nparmigiano <128658656+nparmigiano@users.noreply.github.com>
2023-03-22 22:44:10 -0400

    Update Lab 06.py

--- Lab 06.py
+++ Lab 06.py
@@ -41,13 +41,13 @@ while True:
     choice = input("Please enter an option: ")
 
     if choice == "1":
-        password = input("Please enter your 8-digit password to encode: ")
+        password = input("Please enter your password to encode: ")
         try:
             encoded_password = encode_password(password)
         except ValueError as e:
             print(e)
         else:
-            print("Your password has been encoded and stored as: ", encoded_password)
+            print("Your password has been encoded and stored!)
 
     elif choice == "2":
         encoded_password = input("Please enter your password to decode: ")
@@ -56,10 +56,10 @@ while True:
         except ValueError as e:
             print(e)
         else:
-            print("Your password has been decoded and is: ", decoded_password)
-
+            print("The encoded password is", encoded_password, "and the original password is", decoded_password)
+                  
     elif choice == "3":
-        print("Exiting...")
+ 
         break
 
     else:
